16bde040aedc477e4e320231cd7d4cf71da4abe4
Keo <kthorng@gmail.com>
2019-01-09 14:22:24 -0800

    Updates

---
+++ precache-manifest.9ce68a312383713135c076c2cafc82a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd2a89757c096ae903162a4c430e383d",
+    "url": "/clicky-click-click/static/media/drago.dd2a8975.jpg"
+  },
+  {
+    "revision": "5d1168c3dd7ff2f73711",
+    "url": "/clicky-click-click/static/js/runtime~main.5d1168c3.js"
+  },
+  {
+    "revision": "db8d368c114a4770854b",
+    "url": "/clicky-click-click/static/js/main.db8d368c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-click-click/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "db8d368c114a4770854b",
+    "url": "/clicky-click-click/static/css/main.b20170bb.chunk.css"
+  },
+  {
+    "revision": "d9e4c6d456ce144aed861787379550c7",
+    "url": "/clicky-click-click/index.html"
+  }
+];
